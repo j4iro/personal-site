7df67bc2ecacb4b9efb942043edc19bf59a3f073
j4iro <jlachirap.8@gmail.com>
2021-03-10 00:02:22 -0500

    se ha editado home, about y se empezó a cambiar data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,16 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  title: "j4iro",
+  description: "Personal Page, Portafolio and Blog",
+  keywords: [
+    "j4iro",
+    "Jairo Lachira Peralta",
+    "Portafolio",
+    "Software Developer",
+  ],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: "https://vredeburg.netlify.app",
   // how many posts you want to show for each page
-  paginate: 6
+  paginate: 6,
   // if you want to add disqus to your site
   // disqusShortname: "your-shortname"
 };
