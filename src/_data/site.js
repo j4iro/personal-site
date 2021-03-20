ae7c24f0b2b85cbad57e3271318f2d63b2808d4f
j4iro <jlachirap.8@gmail.com>
2021-03-20 16:42:06 -0500

    se cambió tags por skills, se agregó proyecto money manager y textos

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,15 @@
 module.exports = {
-  title: "j4iro",
-  description: "Personal Page, Portafolio and Blog",
+  title: "J4iro",
+  description: "Personal Page, Portfolio and Blog",
   keywords: [
     "j4iro",
     "Jairo Lachira Peralta",
     "Portafolio",
     "Software Developer",
+    "Backend Developer",
   ],
   // your site url without trailing slash
-  url: "https://vredeburg.netlify.app",
+  url: "https://trusting-carson-d5574d.netlify.app/",
   // how many posts you want to show for each page
   paginate: 6,
   // if you want to add disqus to your site
---
+++ src/posts/money-manager.md
@@ -0,0 +1,11 @@
+---
+title: "Api Money Manager"
+date: 2021-03-20T10:00
+thumb: "money-manager.png"
+urlp: "https://github.com/j4iro/money-manager"
+description: "Api hecha en express.js para una aplicación que administrará tus finanzas personales"
+tags:
+  - Express.js
+  - Node.js
+  - MongoDB
+---
